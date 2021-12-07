a518d58f9a370d58a6eea38cf03258290e5ab847
ilwel <ilwelisaac@hotmail.com>
2021-12-07 11:04:25 -0300

    app root

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import './App.css';
+import MyRoutes from './router';
+import sunIcon from './assets/sun-icon.svg';
+import moonIcon from './assets/moon-icon.svg';
+import { useState } from 'react';
 
 function App() {
+  const [darkTheme, setDarkTheme] = useState(false);
+
+  const handleTheme = () => {
+    setDarkTheme(!darkTheme);
+  }
+
   return (
-    <div className="l-app">
-      hello
+    <div className={`l-app ${darkTheme ? 't-dark-mode' : 't-light-mode'}`}>
+      <button onClick={handleTheme} className="set-theme">
+        <img src={darkTheme ? moonIcon : sunIcon} alt="icon" />
+      </button>
+      <MyRoutes />
     </div>
   );
 }
